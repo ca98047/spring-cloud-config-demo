9a3badd0047177616786c45e41409f7ca0c5bbad
최진원 <jinow.choi@musinsa.com>
2021-01-18 20:43:53 +0900

    divide profile

--- src/main/java/com/jinow/springcloudconfigdemo/SpringCloudConfigDemoApplication.java
+++ src/main/java/com/jinow/springcloudconfigdemo/SpringCloudConfigDemoApplication.java
@@ -2,8 +2,10 @@ package com.jinow.springcloudconfigdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigDemoApplication {
 
     public static void main(String[] args) {
